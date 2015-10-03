714b528e379dff0a618cc3adead081df4fed1ca6
Simon Chiu <si@sichiu.co.uk>
2015-10-03 13:13:40 +0100

    Attack our positions first

--- Bot/Bot.cs
+++ Bot/Bot.cs
@@ -11,9 +11,15 @@ namespace Bot
         public int GridLength { get; set; }
         public string GridLetter { get; set; }
         public List<string> Positions;
+        public List<string> MyShipPositions { get; set; }
         public int MaxTurns { get; set; }
         public string[] Ships { get; set; }
         public int MineCount { get; set; }
+
+        public Bot()
+        {
+            MyShipPositions = new List<string>();
+        }
 
         public void SetupGrid(string gridSize, int maxTurns, string[] ships, int mineCount)
         {
@@ -23,11 +29,19 @@ namespace Bot
             SetupGrid(gridSize);
         }
 
+        public void MyShips(string[] myShip)
+        {
+            for (int i = 0; i < myShip.Length; i++)
+            {
+                MyShipPositions.Add(myShip[i]);
+            }
+        }
+
         public void SetupGrid(string gridSize)
         {
             Positions = new List<string>();
             GridLetter = gridSize.Substring(0, 1);
-            GridLength = int.Parse(gridSize.Substring(1, gridSize.Length-1));
+            GridLength = int.Parse(gridSize.Substring(1, gridSize.Length - 1));
 
             for (char c = 'A'; c <= Convert.ToChar(GridLetter); c++)
             {
@@ -36,15 +50,24 @@ namespace Bot
                 {
                     Positions.Add(c + i.ToString());
                 }
-            } 
-
-
+            }
         }
 
         public string NextMove()
         {
-            var move = Positions[0];
+            var move = string.Empty;
+            if (MyShipPositions.Any())
+            {
+                move = MyShipPositions[0];
+                MyShipPositions.Remove(move);
+            }
+            else
+            {
+                move = Positions[0];
+            }
+
             Positions.Remove(move);
+
             return move;
         }
     }
