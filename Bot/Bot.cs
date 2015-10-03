e8412cce6f9751454efdec7b1ca13d5d45b57cd1
Simon Chiu <si@sichiu.co.uk>
2015-10-03 14:27:09 +0100

    Place our ships

--- Bot/Bot.cs
+++ Bot/Bot.cs
@@ -15,10 +15,27 @@ namespace Bot
         public int MaxTurns { get; set; }
         public string[] Ships { get; set; }
         public int MineCount { get; set; }
+        public int GridLengthDifference { get; set; }
+        private int _placedShips;
 
-        public Bot()
+        public List<ShipPosition> MyPlacedShips = new List<ShipPosition>
         {
-            MyShipPositions = new List<string>();
+            new ShipPosition {GridLetter = "B", GridNumber = 4, Orientation = "horizontal"},
+            new ShipPosition {GridLetter = "H", GridNumber = 1, Orientation = "vertical"},
+            new ShipPosition {GridLetter = "A", GridNumber = 3, Orientation = "vertical"},
+            new ShipPosition {GridLetter = "B", GridNumber = 2, Orientation = "horizontal"},
+            new ShipPosition {GridLetter = "G", GridNumber = 4, Orientation = "vertical"},
+            new ShipPosition {GridLetter = "B", GridNumber = 6, Orientation = "horizontal"},
+            new ShipPosition {GridLetter = "A", GridNumber = 8, Orientation = "horizontal"},
+            new ShipPosition {GridLetter = "C", GridNumber = 3, Orientation = "horizontal"},
+        };
+
+        public ShipPosition PlaceShip()
+        {
+            var ship = MyPlacedShips[_placedShips];
+            ship.GridNumber += GridLengthDifference;
+            _placedShips++;
+            return ship;
         }
 
         public void SetupGrid(string gridSize, int maxTurns, string[] ships, int mineCount)
@@ -39,13 +56,15 @@ namespace Bot
 
         public void SetupGrid(string gridSize)
         {
+            MyShipPositions = new List<string>();
+            _placedShips = 0;
             Positions = new List<string>();
             GridLetter = gridSize.Substring(0, 1);
             GridLength = int.Parse(gridSize.Substring(1, gridSize.Length - 1));
+            GridLengthDifference = GridLength > 8 ? GridLength - 8 : 0;
 
             for (char c = 'A'; c <= Convert.ToChar(GridLetter); c++)
             {
-                //do something with letter 
                 for (var i = 1; i < GridLength + 1; i++)
                 {
                     Positions.Add(c + i.ToString());
@@ -70,5 +89,12 @@ namespace Bot
 
             return move;
         }
+    }
+
+    public class ShipPosition
+    {
+        public string GridLetter { get; set; }
+        public int GridNumber { get; set; }
+        public string Orientation { get; set; }
     }
 }
