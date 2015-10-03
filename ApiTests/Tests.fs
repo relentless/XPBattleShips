3ef5b1956c36ea3fb4c83c812166db4ea407a8a4
Grant Crofton <grantac@hotmail.com>
2015-10-03 11:51:17 +0100

    Added files

---
+++ ApiTests/Tests.fs
@@ -0,0 +1,35 @@
+﻿module Tests
+
+open NUnit.Framework
+open FsUnit
+open HttpClient
+open Nancy
+open Nancy.Extensions
+open Nancy.Hosting.Self
+open System
+
+let hostConfig = new HostConfiguration()
+hostConfig.AllowChunkedEncoding <- false
+// Enabling chunked encoding breaks HEAD requests if you're self-hosting.
+// It also seems to mean the Content-Length isn't set in some cases.
+hostConfig.UrlReservations<-UrlReservations(CreateAutomatically=true)
+
+let nancyHost = 
+    new NancyHost(
+        hostConfig, 
+        new Uri("http://localhost:80/TestServer/"))
+
+[<TestFixture>]
+type ``Integration tests`` ()=
+
+    [<TestFixtureSetUp>]
+    member x.fixtureSetup() =
+        nancyHost.Start()
+
+    [<Test>]
+    // This needs to be run first, as the keep-alive is only set on the first call.  They seem to be run alphabetically.
+    member x.``_if KeepAlive is true, Connection set to 'Keep-Alive' on the first request, but not subsequent ones`` () =
+
+        let response = createRequest Get "http://localhost/Bot/START" |> getResponseCode
+        response |> should equal 200
+
---
+++ WebAPI/BotServer.fs
@@ -0,0 +1,105 @@
+﻿open Nancy
+open Nancy.Extensions
+open Nancy.Hosting.Self
+open System
+open Bot
+
+let bot = Bot()
+
+type FakeServer() as self = 
+    inherit NancyModule()
+
+//    let mutable places = 
+//        [for index in 1..8 do
+//            for char in 'A'..'H' do
+//                yield string char + string index]
+
+    do
+        self.Get.["/"] <- 
+            fun _ -> 
+                //let requestBody = self.Request.Body.AsString()
+                printfn "Index\n"
+                let response = "Grant and Simon's Bot" |> Nancy.Response.op_Implicit 
+                response.StatusCode <- HttpStatusCode.OK
+                response :> obj
+
+        self.Post.["/START"] <- 
+            fun _ -> 
+                let requestBody = self.Request.Body.AsString()
+                printfn "START\n%s" requestBody
+                //let response = "Response" |> Nancy.Response.op_Implicit 
+                //response.StatusCode <- HttpStatusCode.OK
+                200 :> obj
+
+        self.Get.["/PLACE"] <- 
+            fun _ -> 
+                let requestBody = self.Request.Body.AsString()
+                printfn "PLACE (Get)\n%s" requestBody
+                let response = "Response" |> Nancy.Response.op_Implicit 
+                response.StatusCode <- HttpStatusCode.OK
+                200 :> obj
+
+        self.Post.["/PLACE"] <- 
+            fun _ -> 
+                let requestBody = self.Request.Body.AsString()
+                printfn "PLACE (Post)\n%s" requestBody
+                200 :> obj
+
+        self.Post.["/SCAN"] <- 
+            fun _ -> 
+                let requestBody = self.Request.Body.AsString()
+                printfn "SCAN\n%s" requestBody
+                200 :> obj
+
+        self.Get.["/MOVE"] <- 
+            fun _ -> 
+                let requestBody = self.Request.Body.AsString()
+                printfn "MOVE\n%s" requestBody
+
+                
+                let place = bot.NextMove()
+
+                let response = "{\"type\": \"ATTACK\", \"gridReference\" : \"" + place + "\"}" |> Nancy.Response.op_Implicit 
+                response.StatusCode <- HttpStatusCode.OK
+                response.ContentType <- "application/json"
+                response :> obj
+
+        self.Post.["/HIT"] <- 
+            fun _ -> 
+                let requestBody = self.Request.Body.AsString()
+                printfn "HIT\n%s" requestBody
+                200 :> obj
+
+        self.Post.["/HIT_MINE"] <- 
+            fun _ -> 
+                let requestBody = self.Request.Body.AsString()
+                printfn "HIT_MINE\n%s" requestBody
+                200 :> obj
+
+        self.Post.["/MISS"] <- 
+            fun _ -> 
+                let requestBody = self.Request.Body.AsString()
+                printfn "MISS\n%s" requestBody
+                //let response = "Response" |> Nancy.Response.op_Implicit 
+                //response.StatusCode <- HttpStatusCode.OK
+                200 :> obj
+
+[<EntryPoint>]
+let main argv = 
+    
+    let hostConfig = new HostConfiguration()
+    hostConfig.UrlReservations<-UrlReservations(CreateAutomatically=true)
+
+    let nancyHost = 
+        new NancyHost(
+            hostConfig, 
+            new Uri("http://localhost:80/Bot/"))
+
+    nancyHost.Start()
+
+    printfn "Nancy host running on http://localhost:80/Bot"
+
+    Console.ReadLine()
+    
+
+    0 // return an integer exit code
